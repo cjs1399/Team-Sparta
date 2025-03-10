c695563086c4063b85d3b8405b74aa383a15b5c1
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-10 17:51:10 +0900

    [Feat] #1 - 1에서 9까지의 서로 다른 임의의 수 3개를 생성하는 기능을 구현합니다.

---
+++ SpartaBaseballGame/SpartaBaseballGame/BaseballGameLv1.swift
@@ -0,0 +1,18 @@
+//
+//  BaseballGameLv1.swift
+//  SpartaBaseballGame
+//
+//  Created by 천성우 on 3/10/25.
+//
+
+import Foundation
+
+func makeRandomNumbers() -> [Int] {
+    // baseballGameLV1
+    // 조건1: 1에서 9까지의 서로 다른 임의의 수 3개를 정한다.
+    // 조건2: 랜덤으로 만들어진다
+    return Array(1...9).shuffled().prefix(3).map { $0 }
+    
+    // Array(1...9).shuffled()를 사용하면 1~9까지의 숫자를 무작위로 섞어 배열 반환
+    // prefix(3)을 이용해 위에서 만들어진 배열에서 앞에서 3개의 숫자를 추출
+}
