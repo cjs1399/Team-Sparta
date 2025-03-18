b71fabd0855fac0e26c942811c554efb1b7bb73f
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-18 16:47:53 +0900

    [Fix] - code Fix

--- Sparta/Sparta/Global/Literals/SizeLiterals.swift
+++ Sparta/Sparta/Global/Literals/SizeLiterals.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 struct SizeLiterals {
-    
     struct Screen {
         static let screenWidth: CGFloat = UIScreen.main.bounds.width
         static let screenHeight: CGFloat = UIScreen.main.bounds.height
