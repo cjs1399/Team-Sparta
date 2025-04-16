5f8bbf41cceeb9dd833082e83399ff4eff512631
cjs1399 <cjs1399@naver.com>
2025-04-16 16:21:50 +0900

    [Feat] #18 - 나라이름을 표시하기 위한 subTitleLabel 추가

--- ExchangeRate/ExchangeRate/Presentation/ExchangeRate/Cell/ExchangeRateTableViewCell.swift
+++ ExchangeRate/ExchangeRate/Presentation/ExchangeRate/Cell/ExchangeRateTableViewCell.swift
@@ -15,6 +15,7 @@ import Then
 class ExchangeRateTableViewCell: BaseTableViewCell {
     
     private let titleLabel = UILabel()
+    private let subTitleLabel = UILabel()
     private let priceLabel = UILabel()
     
     override func setStyles() {
@@ -22,16 +23,25 @@ class ExchangeRateTableViewCell: BaseTableViewCell {
             $0.textColor = .black
         }
         
+        subTitleLabel.do {
+            $0.textColor = .gray
+            $0.font = .systemFont(ofSize: 12, weight: .regular)
+        }
+        
         priceLabel.do {
             $0.textColor = .black
         }
     }
     override func setLayout() {
-        contentView.addSubviews(titleLabel, priceLabel)
+        contentView.addSubviews(titleLabel, subTitleLabel, priceLabel)
         
         titleLabel.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().offset(10)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(5)
             $0.leading.equalToSuperview().offset(10)
-            $0.centerY.equalToSuperview()
         }
         
         priceLabel.snp.makeConstraints {
@@ -40,8 +50,9 @@ class ExchangeRateTableViewCell: BaseTableViewCell {
         }
     }
     
-    func configure(title: String, price: Double) {
+    func configure(title: String, subTitle: String, price: Double) {
         titleLabel.text = title
-        priceLabel.text = "\(price)"
+        subTitleLabel.text = subTitle
+        priceLabel.text = String(format: "%.4f", price)
     }
 }
