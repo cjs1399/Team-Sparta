ef84f7ad9a5daa6e47ffd291a30ae3ae8053ce67
cjs1399 <cjs1399@naver.com>
2025-04-22 17:55:28 +0900

    [Feat] #18 - AppDependencyFactory 수정

--- ExchangeRate/ExchangeRate/Application/AppDependencyFactory.swift
+++ ExchangeRate/ExchangeRate/Application/AppDependencyFactory.swift
@@ -10,33 +10,43 @@ import UIKit
 import CoreData
 
 final class AppDependencyFactory {
-    
+
     static func makeExchangeRateViewModel() -> ExchangeRateViewModel {
         let service = ExchangeRateService()
-        let repository = ExchangeRateRepositoryImpl(service: service)
-        let useCase = FetchExchangeRateUseCaseImpl(repository: repository)
+        let exchangeRateRepo = ExchangeRateRepositoryImpl(service: service)
+        let fetchUseCase = FetchExchangeRateUseCaseImpl(repository: exchangeRateRepo)
 
         let favoriteRepo = FavoriteCurrencyRepository(context: CoreDataStack.shared.context)
         let fetchFavoriteUseCase = FetchFavoriteCurrenciesUseCaseImpl(repository: favoriteRepo)
         let toggleFavoriteUseCase = ToggleFavoriteCurrencyUseCaseImpl(repository: favoriteRepo)
 
-        return ExchangeRateViewModel(
-            fetchUseCase: useCase,
-            fetchFavoriteUseCase: fetchFavoriteUseCase,
-            toggleFavoriteUseCase: toggleFavoriteUseCase
+        let cachedRepo = CachedExchangeRateRepository(context: CoreDataStack.shared.context)
+        let compareUseCase = CompareExchangeRateUseCaseImpl(repository: cachedRepo)
 
+        return ExchangeRateViewModel(
+            fetchUseCase: fetchUseCase,
+            fetchFavoriteUseCase: fetchFavoriteUseCase,
+            toggleFavoriteUseCase: toggleFavoriteUseCase,
+            compareUseCase: compareUseCase
         )
     }
 
     static func makeExchangeCalculatorViewModel(item: ExchangeRateItemDisplay) -> ExchangeCalculatorViewModel {
         return ExchangeCalculatorViewModel(selectedItem: item)
     }
-    
+
+    // MARK: - Repository / UseCase 개별 생성
+
     static func makeFavoriteCurrencyRepository() -> FavoriteCurrencyRepositoryInterface {
         return FavoriteCurrencyRepository(context: CoreDataStack.shared.context)
     }
 
     static func makeFetchFavoriteCurrenciesUseCase() -> FetchFavoriteCurrenciesUseCase {
         return FetchFavoriteCurrenciesUseCaseImpl(repository: makeFavoriteCurrencyRepository())
+    }
+
+    static func makeCompareExchangeRateUseCase() -> CompareExchangeRateUseCase {
+        let cachedRepo = CachedExchangeRateRepository(context: CoreDataStack.shared.context)
+        return CompareExchangeRateUseCaseImpl(repository: cachedRepo)
     }
 }
